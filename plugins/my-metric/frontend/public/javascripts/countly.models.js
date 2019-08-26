ecc6d0b8fe564a06999b430083e10eae4f920501
promil bhardwaj <promilbhardwaj.nith@gmail.com>
2019-08-26 20:06:38 +0000

    model base file added for plugin

---
+++ plugins/my-metric/frontend/public/javascripts/countly.models.js
@@ -0,0 +1,33 @@
+(function (myMetric, $) {
+
+    //we will store our data here
+    var _data = {};
+
+    //Initializing model
+    myMetric.initialize = function () {
+
+        //returning promise
+        return $.ajax({
+            type: "GET",
+            url: "/o",
+            data: {
+                //providing current user's api key
+                "api_key": countlyGlobal.member.api_key,
+                //providing current app's id
+                "app_id": countlyCommon.ACTIVE_APP_ID,
+                //specifying method param
+                "method": "ourplugin"
+            },
+            success: function (json) {
+                //got our data, let's store it
+                _data = json;
+            }
+        });
+    };
+
+    //return data that we have
+    myMetric.getData = function () {
+        return _data;
+    };
+
+}(window.myMetric = window.myMetric || {}, jQuery));
