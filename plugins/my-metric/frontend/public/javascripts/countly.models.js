9c6cedaca21afaf3985a2806f29d713076d08dc6
promil <promil.bhardwaj@vyom.com>
2019-08-28 00:04:23 +0530

    trying out a custom view file

--- plugins/my-metric/frontend/public/javascripts/countly.models.js
+++ plugins/my-metric/frontend/public/javascripts/countly.models.js
@@ -1,33 +1,31 @@
-// (function(myMetric, $) {
-//     //we will store our data here
-//     var _data = {};
+(function(myMetric, $) {
+    //we will store our data here
+    var _data = {};
 
-//     //Initializing model
-//     myMetric.initialize = function() {
-//         //returning promise
-//         return $.ajax({
-//             type: "GET",
-//             url: "/o",
-//             data: {
-//                 //providing current user's api key
-//                 api_key: countlyGlobal.member.api_key,
-//                 //providing current app's id
-//                 app_id: countlyCommon.ACTIVE_APP_ID,
-//                 //specifying method param
-//                 method: "ourplugin"
-//             },
-//             success: function(json) {
-//                 //got our data, let's store it
-//                 _data = json;
-//             }
-//         });
-//     };
+    //Initializing model
+    myMetric.initialize = function() {
+        //returning promise
+        return $.ajax({
+            type: "GET",
+            url: "/o/my-metric",
+            data: {
+                //providing current user's api key
+                api_key: countlyGlobal.member.api_key,
+                //providing current app's id
+                app_id: countlyCommon.ACTIVE_APP_ID,
+                //specifying method param
+                method: "myMetric"
+            },
+            success: function(json) {
+                //got our data, let's store it
+                console.log(json);
+                _data = json;
+            }
+        });
+    };
 
-//     //return data that we have
-//     myMetric.getData = function() {
-//         return _data;
-//     };
-// })((window.myMetric = window.myMetric || {}), jQuery);
-
-
-CountlyHelpers.createMetricModel(window.myMetric = window.myMetric || {}, "myMetric", jQuery);
+    //return data that we have
+    myMetric.getData = function() {
+        return _data || {};
+    };
+})((window.myMetric = window.myMetric || {}), jQuery);
--- plugins/my-metric/frontend/public/javascripts/countly.views.js
+++ plugins/my-metric/frontend/public/javascripts/countly.views.js
@@ -1,87 +1,44 @@
 window.myMetricView = countlyView.extend({
     //initalize out model
     beforeRender: function() {
-        return $.when(myMetric.initialize()).then(function() {});
+        if (this.template) {
+            return $.when(myMetric.initialize()).then(function() {});
+        } else {
+            var self = this;
+            return $.when(
+                $.get(
+                    countlyGlobal.path + "/my-metric/templates/my-metric.html",
+                    function(src) {
+                        self.template = Handlebars.compile(src);
+                    }
+                ),
+                myMetric.initialize()
+            ).then(function() {});
+        }
     },
 
-    //render our data
     renderCommon: function(isRefresh) {
-        var data = myMetric.getData();
-
-        //prepare template data
+        var crashData = myMetric.getData();
+        var chartData = myMetric.getChartData(
+            this.curMetric,
+            this.metrics[this.curMetric]
+        );
+        var dashboard = myMetric.getDashboardData();
         this.templateData = {
-            "page-title": jQuery.i18n.map["ourmetric.title"],
-            "logo-class": "",
-            "graph-type-double-pie": true,
-            "pie-titles": {
-                left: jQuery.i18n.map["common.total-users"],
-                right: jQuery.i18n.map["common.new-users"]
-            }
+            "page-title": jQuery.i18n.map["myMetric.title"],
+            "no-data": jQuery.i18n.map["common.bar.no-data"]
         };
 
-        //if loading first time and not refershing
+        var self = this;
         if (!isRefresh) {
-            //build template with data
+            countlyCommon.drawTimeGraph(chartData.chartDP, "#dashboard-graph");
+            chartData = myMetric.getChartData(
+                self.curMetric,
+                self.metrics[self.curMetric],
+                self.showOnGraph
+            );
             $(this.el).html(this.template(this.templateData));
-
-            //create datatable with chart data
-            this.dtable = $(".d-table").dataTable(
-                $.extend({}, $.fn.dataTable.defaults, {
-                    //provide data to datatables
-                    aaData: data.chartData,
-
-                    //specify which columns to show
-                    aoColumns: [
-                        {
-                            mData: "ourmetric",
-                            sType: "session-duration",
-                            sTitle: jQuery.i18n.map["ourmetric.title"]
-                        },
-                        {
-                            mData: "t",
-                            sType: "formatted-num",
-                            mRender: function(d) {
-                                return countlyCommon.formatNumber(d);
-                            },
-                            sTitle:
-                                jQuery.i18n.map["common.table.total-sessions"]
-                        },
-                        {
-                            mData: "u",
-                            sType: "formatted-num",
-                            mRender: function(d) {
-                                return countlyCommon.formatNumber(d);
-                            },
-                            sTitle: jQuery.i18n.map["common.table.total-users"]
-                        },
-                        {
-                            mData: "n",
-                            sType: "formatted-num",
-                            mRender: function(d) {
-                                return countlyCommon.formatNumber(d);
-                            },
-                            sTitle: jQuery.i18n.map["common.table.new-users"]
-                        }
-                    ]
-                })
-            );
-
-            //make table headers sticky
-            $(".d-table").stickyTableHeaders();
-
-            //draw chart with total data
-            countlyCommon.drawGraph(
-                data.chartDPTotal,
-                "#dashboard-graph",
-                "pie"
-            );
-
-            //draw chart with new data
-            countlyCommon.drawGraph(
-                data.chartDPNew,
-                "#dashboard-graph2",
-                "pie"
-            );
+            self.switchMetric();
         }
     },
 
@@ -127,19 +84,18 @@ window.myMetricView = countlyView.extend({
 app.myMetricView = new myMetricView();
 
 //register route
-app.route("/my-metric", "my-metric", function () {
+app.route("/my-metric", "my-metric", function() {
     this.renderWhenReady(this.myMetricView);
 });
 
-
-$(document).ready(function () {
-    var menu = '<a href="#/my-metric" class="item" ">' +
+$(document).ready(function() {
+    var menu =
+        '<a href="#/my-metric" class="item" ">' +
         '<div class="logo fa fa-cubes" style="background-image:none; font-size:24px; text-align:center; width:35px; margin-left:14px; line-height:42px;"></div>' +
         '<div class="text" data-localize="my-metric.title">My Metric</div>' +
-        '</a>';
+        "</a>";
 
-    if ($('.sidebar-menu #management-menu').length)
-        $('.sidebar-menu #management-menu').before(menu);
-    else
-        $('.sidebar-menu').append(menu);
+    if ($(".sidebar-menu #management-menu").length)
+        $(".sidebar-menu #management-menu").before(menu);
+    else $(".sidebar-menu").append(menu);
 });
